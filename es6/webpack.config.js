06b8cb4909cb47d006f976b26b6121f17438cbc1
killlmoves <baruri60@gmail.com>
2017-10-19 18:51:10 +0530

    First commit

---
+++ es6/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: ['babel-polyfill', './app/index.js'],
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  // NOTE 'Setting up Webpack'
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, // a regular expression that catches .js files
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    port: 3000, // most common port
+    contentBase: './build',
+    inline: true
+  }
+  // NOTE 'Adding babel to Webpack'
+}
